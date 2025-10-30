80b82914b681c0eed58fe91a709d981777cafc7c
User <shounbabu31@gmail.com>
2025-10-30 10:38:23 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
      // home: SplashScreen(),
 
-      home: HomeScreen(),
+      home: Homepage1(),
     );
   }
 }
